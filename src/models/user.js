cb3550bafba2d8b9f2ef0dab61ebb855d3ba9a1e
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2024-02-11 22:03:49 +0500

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    username : {
+        type : String,
+        require : true
+    },
+    email : {
+        type : String,
+        require : true
+    },
+    password : {
+        type : String,
+        require : true
+    },
+    confirmPassword : {
+        type : String,
+        require : false
+    },
+    
+    cnic : {
+        type: Number,
+        require: true
+    },
+    address : {
+        type: String,
+        require: true
+    },
+
+    verified : {
+        type : Boolean,
+        default : false
+    },
+    otp: {
+        type: Number,
+        require: false
+    }
+}, {timestamps : true});
+
+module.exports = mongoose.model("User", UserSchema);
